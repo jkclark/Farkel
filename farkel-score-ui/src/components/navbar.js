27bf904be2e4b45a429a9e16e9afcb0acee55a20
Josh Clark <jclarktennis@gmail.com>
2022-09-29 11:23:49 -0400

    All 'pages' now leave room for navbar.

--- farkel-score-ui/src/components/navbar.js
+++ farkel-score-ui/src/components/navbar.js
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import "./navbar.css";
+
 function MainNavbar() {
   return (
     <Navbar expand="sm" bg="light" fixed="top">
