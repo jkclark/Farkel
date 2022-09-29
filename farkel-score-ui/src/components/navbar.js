ddfa608256a490f4e945dfcff0340e6b6b2e3b65
Josh Clark <jclarktennis@gmail.com>
2022-09-29 13:08:40 -0400

    Added 'FAQ' and 'Contact' modals, but their content isn't complete.

--- farkel-score-ui/src/components/navbar.js
+++ farkel-score-ui/src/components/navbar.js
@@ -1,23 +1,82 @@
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import "./navbar.css";
 
-function MainNavbar() {
+function CustomModal(props) {
+  const { title, body } = props.contents;
   return (
-    <Navbar expand="sm" bg="light" fixed="top">
-      <Container>
-        <Navbar.Brand>Farkel Score</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link>FAQ</Nav.Link>
-            <Nav.Link>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Modal show={props.show} onHide={props.onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{body}</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function MainNavbar() {
+  const [showModal, setShowModal] = useState(false);
+  const [modalContents, setModalContents] = useState({});
+  const FAQ_CONTENTS = {
+    title: "FAQ",
+    body: "FAQ body here",
+  };
+  const CONTACT_CONTENTS = {
+    title: "Contact me",
+    body: "Contact me body here",
+  };
+
+  return (
+    <>
+      <Navbar expand="sm" bg="light" fixed="top">
+        <Container>
+          <Navbar.Brand>Farkel Score</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link
+                onClick={(event) => {
+                  event.preventDefault();
+
+                  setModalContents(FAQ_CONTENTS);
+
+                  setShowModal(true);
+                }}
+              >
+                FAQ
+              </Nav.Link>
+              <Nav.Link
+                onClick={(event) => {
+                  event.preventDefault();
+
+                  setModalContents(CONTACT_CONTENTS);
+
+                  setShowModal(true);
+                }}
+              >
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <CustomModal
+        contents={modalContents}
+        show={showModal}
+        onHide={() => {
+          setShowModal(false);
+        }}
+      ></CustomModal>
+    </>
   );
 }
 
